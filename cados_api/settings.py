fca87c6ee508e0e94b4595d82dccb4f068b1b77a
macourage18 <macourage17@gmail.com>
2024-01-04 17:09:21 +0100

    new

--- cados_api/settings.py
+++ cados_api/settings.py
@@ -203,4 +203,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://192.168.1.179:3000",
+]
