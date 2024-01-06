7d20a824490131d0b9c3f5e18d67d67e4c18d953
macourage18 <macourage17@gmail.com>
2024-01-06 03:26:55 +0100

    new

--- base/urls.py
+++ base/urls.py
@@ -6,8 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -26,4 +26,5 @@ urlpatterns = [
   #compianies id
   path('companies/<str:name>/', views.companies_details)
 ] 
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
